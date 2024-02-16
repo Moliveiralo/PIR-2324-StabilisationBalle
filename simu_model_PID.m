cca794d4a41f16cbf36a228701b6950a2bb2ac29
LucieVorms <151724807+LucieVorms@users.noreply.github.com>
2024-02-16 12:06:32 +0100

    Début modélisation systeme

---
+++ simu_model_PID.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Simulation du système plateau + ball %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Données du système 
+ m = 2.7; % Masse de la balle en g
+ r = 0.022; % Rayon de la balle en m
+ g = 9.81; % en m.s^-2
+ x %position de la balle sur le plateau sur l'axe x
+ y %position de la balle sur le plateau sur l'axe y
+ 
+ %Entrées du systeme
+ theta_x %angle du plateau avec l'axe x => Beta 
+ theta_y %angle du plateau avec l'axe y => alpha
+ 
+ tau_x %couple appliqué a l'axe x
+ tau_y %couple appliqué a l'axe y
+ J %moment d'inertie de la balle
+ Jp %moment d'inertie du plateau
+ 

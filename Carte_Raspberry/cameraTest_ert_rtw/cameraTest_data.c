592b7393816a1abd37d896b7e16bd8e293906f37
MaximeO <133717115+Moliveiralo@users.noreply.github.com>
2024-02-27 15:09:12 +0100

    Add files via upload

---
+++ Carte_Raspberry/cameraTest_ert_rtw/cameraTest_data.c
@@ -0,0 +1,34 @@
+/*
+ * Classroom License -- for classroom instructional use only.  Not for
+ * government, commercial, academic research, or other organizational use.
+ *
+ * File: cameraTest_data.c
+ *
+ * Code generated for Simulink model 'cameraTest'.
+ *
+ * Model version                  : 1.5
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Tue Feb 27 14:46:09 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "cameraTest.h"
+#include "cameraTest_private.h"
+
+/* Constant parameters (default storage) */
+const ConstP_cameraTest_T cameraTest_ConstP = {
+  /* Expression: devName
+   * Referenced by: '<Root>/V4L2 Video Capture'
+   */
+  { 47U, 100U, 101U, 118U, 47U, 118U, 105U, 100U, 101U, 111U, 50U, 0U }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ Carte_Raspberry/cameraTest_ert_rtw/cameraTest_private.h
@@ -0,0 +1,34 @@
+/*
+ * Classroom License -- for classroom instructional use only.  Not for
+ * government, commercial, academic research, or other organizational use.
+ *
+ * File: cameraTest_private.h
+ *
+ * Code generated for Simulink model 'cameraTest'.
+ *
+ * Model version                  : 1.5
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Tue Feb 27 14:46:09 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_cameraTest_private_h_
+#define RTW_HEADER_cameraTest_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmSetTFinal
+# define rtmSetTFinal(rtm, val)        ((rtm)->Timing.tFinal = (val))
+#endif
+#endif                                 /* RTW_HEADER_cameraTest_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */

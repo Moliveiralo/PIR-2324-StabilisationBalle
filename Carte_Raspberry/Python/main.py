1606b06b5e9f9a19a49698dbb9bc7fc5d9b0d60f
MaximeO <maxime.oliveira40@gmail.com>
2024-04-29 17:28:05 +0200

    Restructuration du code, ajout de l'en-tête, commentaires sur le code déjà existant

--- Carte_Raspberry/Python/main.py
+++ Carte_Raspberry/Python/main.py
@@ -1,34 +1,64 @@
-#Import pour la camera Raspberry
-from io import BytesIO
-from picamera import PiCamera
-from time import sleep
+###########################################################
+#                                                         #
+#                 Stabilisation de balle                  #
+#                                                         #
+#            Auteurs : OLIVEIRA LOPES Maxime              #
+#            NAJI Inès                                    #
+#            COUSTON Emma                                 #
+#            BIGOT Timothé                                #
+#            VORMS Lucie                                  #
+#            BEGHIN Léa                                   #
+#            Date de création: 27/04/2024                 #
+#            Description :                                #
+#            - Description brève du projet                #
+#                                                         #
+###########################################################
 
-#Objet permettant de stocker la video en mémoire
-stream = BytesIO()
 
-#Déclaration de la caméra
+# -------------------------------------------------------------- #
+##### --------------------- LIBRAIRIES --------------------- #####
+# -------------------------------------------------------------- #
+
+import io  # Pour gérer les flux d'octets en mémoire -- Documentation : https://docs.python.org/3/library/io.html
+from picamera import PiCamera  # Pour utiliser la caméra Raspberry Pi -- Documentation : https://picamera.readthedocs.io/en/release-1.13/
+import time as t  # Pour introduire des délais -- Documentation : https://docs.python.org/fr/3/library/time.html
+import PiGPIO as gpio # Librairie pour gérer les GPIO du Raspberry Pi -- Documentation : https://github.com/joan2937/pigpio
+# Si jamais on veut, pour implémenter une interface homme-machine via la raspi: https://wiki.python.org/moin/PyQt
+
+
+# -------------------------------------------------------------- #
+##### --------------- ACQUISITION DE L'IMAGE --------------- #####
+# -------------------------------------------------------------- #
+
+# Initialisation d'un flux pour stocker la vidéo en mémoire
+stream = io.BytesIO()
+
+# Instanciation de l'objet caméra
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (640, 480)  # Définition de la résolution de la caméra
 
-#Voir ce que la camera voit
+# Activation de l'aperçu pour visualiser ce que la caméra capte
 camera.start_preview()
-#Enregistrement
-camera.start_recording(stream, format='h264', quality=23)
-camera.wait_recording(15)
-camera.stop_recording()
-sleep(5)
-camera.stop_preview()
+
+# Démarrage de l'enregistrement vidéo
+camera.start_recording(stream, format='h264', quality=23)  # Format h264 avec une qualité de 23
+camera.wait_recording(15)  # Enregistrement pendant 15 secondes
+camera.stop_recording()  # Arrêt de l'enregistrement
+t.sleep(5)  # Pause de 5 secondes
+camera.stop_preview()  # Fermeture de l'aperçu
+
 
 
 
 ##Les sliders du code de JohanLink servent à modifier sur une interface les coefs du PID, possibilité de l'implémenter plus tard mais c'est pas la priorité
 def PIDcontrol(X_ball, Y_ball, Xball_precedente, Yball_precedente, Xconsigne, Yconsigne):
-    Kp = 0
+    # C(s) = Kp * e(s) + Ki/s * e(s) + Kd * s * e(s) --> Kp, Ki et Kd sont les coefficients du PID, que l'on peut ajuster
+    Kp = 1
     Ki = 0
     Kd = 0
 
-    S_erreurX += Xconsigne - X_ball
-    S_erreurY += Yconsigne - Y_ball
+    S_erreurX += Xconsigne - X_ball # Mise à jour de la somme de l'erreur selon l'axe X
+    S_erreurY += Yconsigne - Y_ball # Mise à jour de la somme de l'erreur selon l'axe Y
 
     Ix=Kp*(Xconsigne - X_ball) + Ki*S_erreurX + Kd*((Xball_precedente - X_ball)/0.0333)
     Iy=Kp*(Yconsigne - Y_ball) + Ki*S_erreurY + Kd*((Yball_precedente - Y_ball)/0.0333)
